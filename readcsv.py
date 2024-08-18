3a10248ccf7882109454393f977a4a6515b42bdc
Dungdzqua <140942432+Dungdzqua@users.noreply.github.com>
2024-08-18 23:55:51 +0700

    shuffle csv

--- readcsv.py
+++ readcsv.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 mycsv=pd.read_csv('data2.csv')
 df=pd.DataFrame(mycsv)
-csvList=df.values
+shuffled_df = df.sample(n=len(df))
+shuffled_df = shuffled_df.reset_index(drop=True)
+csvList=shuffled_df.values
